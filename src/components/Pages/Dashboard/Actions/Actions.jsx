da4810930846bfce57ba4d55233ad52ab59dc76f
mauro sala <mauro.sala@gmail.com>
2022-06-20 09:43:59 +0200

    fix(dashbaord): always show actions

--- src/components/Pages/Dashboard/Actions/Actions.jsx
+++ src/components/Pages/Dashboard/Actions/Actions.jsx
@@ -10,42 +10,33 @@ import Deployment from '../../../../assets/dashboard/deployment.svg'
 const Actions = ({ endpoint, template, deployment }) => (
   <div className={css.Container}>
     <div className={css.Title}>action needed</div>
-
     <ul className={css.UlList}>
-      {(endpoint.list || []).length > 0 && (
-        <li
-          {...((endpoint.list || []).length > 1 && { 'data-completed': true })}
-        >
-          <img src={Endpoint} alt='Add Endpoint' />
-          <Link to='/settings/endpoints' className={css.Link}>
-            add endpoint
-          </Link>
-        </li>
-      )}
-      {(template.list || []).length > 0 && (
-        <li
-          {...((endpoint.list || []).length === 1 && { disabled: true })}
-          {...((template.list || []).length > 1 && { 'data-completed': true })}
-        >
-          <img src={Template} alt='Import Template' />
-          <Link to='/register' className={css.Link}>
-            import template
-          </Link>
-        </li>
-      )}
-      {(deployment.list || []).length > 0 && (
-        <li
-          {...((template.list || []).length === 0 && { disabled: true })}
-          {...((deployment.list || []).length > 0 && {
-            'data-completed': true
-          })}
-        >
-          <img src={Deployment} alt='Add Deployment' />
-          <Link to='/templates' className={css.Link}>
-            publish first deployment
-          </Link>
-        </li>
-      )}
+      <li {...((endpoint.list || []).length > 1 && { 'data-completed': true })}>
+        <img src={Endpoint} alt='Add Endpoint' />
+        <Link to='/settings/endpoints' className={css.Link}>
+          add endpoint
+        </Link>
+      </li>
+      <li
+        {...((endpoint.list || []).length === 1 && { disabled: true })}
+        {...((template.list || []).length > 1 && { 'data-completed': true })}
+      >
+        <img src={Template} alt='Import Template' />
+        <Link to='/register' className={css.Link}>
+          import template
+        </Link>
+      </li>
+      <li
+        {...((template.list || []).length === 0 && { disabled: true })}
+        {...((deployment.list || []).length > 0 && {
+          'data-completed': true
+        })}
+      >
+        <img src={Deployment} alt='Add Deployment' />
+        <Link to='/templates' className={css.Link}>
+          publish first deployment
+        </Link>
+      </li>
     </ul>
   </div>
 )
