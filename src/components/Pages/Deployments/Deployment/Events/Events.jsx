530ba4b1678a7512c5e4790d2c094e4875e1b3ff
mauro sala <mauro.sala@gmail.com>
2022-10-21 15:08:36 +0200

    fix(events): map

--- src/components/Pages/Deployments/Deployment/Events/Events.jsx
+++ src/components/Pages/Deployments/Deployment/Events/Events.jsx
@@ -22,9 +22,6 @@ const Events = ({ deploy, log }) => {
     }
   }, [deploy.metadata.uid, dispatch])
 
-  console.log(log.data)
-  console.log(deploy)
-
   return (
     <React.Fragment>
       <LocalSearch>
