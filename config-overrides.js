40ab92da86eb6608551773e94297b8aeb6c689c5
Diego Braga <diego.braga86@gmail.com>
2023-07-21 17:36:31 +0200

    Adding authentication for websocket

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,16 @@
 // config-overrides.js
-const { override, addWebpackAlias } = require('customize-cra');
+const { override, addWebpackAlias, addWebpackPlugin } = require('customize-cra');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = override(
   addWebpackAlias({
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
-  })
+  }),
+  addWebpackPlugin(
+    new webpack.DefinePlugin({
+      'process.env.JWT_SECRET_KEY': JSON.stringify(process.env.JWT_SECRET_KEY),
+    })
+  )
 );
